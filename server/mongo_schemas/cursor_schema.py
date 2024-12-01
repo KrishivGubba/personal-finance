7d9ca44285aac3c0ea1efe037ac7f76af4fbe874
Krishiv Gubba <kgubba@wisc.edu>
2024-11-30 22:19:53 -0600

    add Cursor db mechanisms

--- server/mongo_schemas/cursor_schema.py
+++ server/mongo_schemas/cursor_schema.py
@@ -8,4 +8,19 @@ class Cursor:
     cursor_type: Literal["transactions"]
 
     def to_dict(self):
-        return asdict(self)
+        return {
+            "access_token": self.access_token,
+            "cursor": self.cursor,
+            "cursor_type": self.cursor_type
+        }
+    
+    @classmethod
+    def from_dict(cls, dict:dict):
+        required_fields = ["access_token", "cursor", "cursor_type"]
+        if not all(field in dict for field in required_fields):
+            raise ValueError(f"Missing required fields. Expected {required_fields}")
+        return cls(
+            access_token = dict["access_token"],
+            cursor = dict["cursor"],
+            cursor_type = dict["cursor_type"]
+        )
--- server/transaction_repository.py
+++ server/transaction_repository.py
@@ -3,29 +3,32 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 from mongo_schemas.transaction_schema import Transaction
+from mongo_schemas.cursor_schema import Cursor
 import logging
+from typing import Optional
+
 
 load_dotenv()
 
 # cluster = MongoClient(os.getenv("MONGO_CLIENT"))
 
 # db = cluster["plaid-app"]
-# collection = db["transactions"]
+# main_collection = db["transactions"]
 
-# collection.insert_one({"_id":0,"name":"krishiv"})
+# main_collection.insert_one({"_id":0,"name":"krishiv"})
 
 class TransactionRepository:
     """
-    Deal with saving saving documents in monogdb collection "transactions" 
+    Deal with saving saving documents in monogdb main_collection "transactions" 
     """
 
     #TODO: pass in the os.getenv thingy when invoking init ("MONGOCLIENT")
-    def __init__(self, cluster_name, db_name, collection_name) -> None:
-        print("hi ok doing")
+    def __init__(self, cluster_name, db_name, main_collection_name,cursor_collection_name) -> None:
         self.cluster = MongoClient(cluster_name)
         self.db = self.cluster[db_name]
-        self.collection = self.db[collection_name]
-        logging.info(f"Successfully connected to MongoDB collection: {collection_name}")
+        self.main_collection = self.db[main_collection_name]
+        self.cursor_collection = self.db[cursor_collection_name]
+        logging.info(f"Successfully connected to MongoDB main_collection: {main_collection_name}")
     
 
     def save_transactions(self, transaction_array:list[Transaction]) -> bool:
@@ -35,10 +38,23 @@ class TransactionRepository:
             transactions: array of transaction objects
         returns: duh
         """
-        print("definitely doing it now")
-        self.collection.insert_many([i.to_dict() for i in transaction_array])
+        self.main_collection.insert_many([i.to_dict() for i in transaction_array])
         return True
 
+    def save_cursor(self, cursor: Cursor) -> bool:
+        self.cursor_collection.update_one(
+            {"access_token": cursor.access_token},  # find by access_token
+            {"$set": cursor.to_dict()}, # update/insert cursor
+            upsert=True  # create if nonexistent
+        )
+    
+    def find_cursor(self, access_token:str) -> Optional[Cursor]:
+        found = self.cursor_collection.find_one(
+            {"access_token":access_token}
+        )
+        if found:
+            return Cursor.from_dict(found)
+        return None
         
 
 
